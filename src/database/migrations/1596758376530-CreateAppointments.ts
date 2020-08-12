3fd485b1207340078c8babe996fa55eb522bd495
Iorgen Wildrik <w.drik@outlook.com>
2020-08-12 13:12:08 -0300

    feat: add relation with appointments and users

--- src/database/migrations/1596758376530-CreateAppointments.ts
+++ src/database/migrations/1596758376530-CreateAppointments.ts
@@ -8,7 +8,7 @@ export class CreateAppointments1596758376530 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
